042f0f799d79360fcf581e93a477e0fcdce73378
vadyy2014 <vadyy2014@gmail.com>
2022-08-14 10:16:29 -0100

    sum changes in API

---
+++ prisma/migrations/20220813143259_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "veiculos" ALTER COLUMN "ano_fabricacao" SET DATA TYPE TEXT;
--- src/controllers/CreateCompraControllers.ts
+++ src/controllers/CreateCompraControllers.ts
@@ -3,11 +3,10 @@ import { prismaClient } from '../database/prismaClient'
 
 export class CreateCompraControllers {
   async handle(request: Request, response: Response) {
-    const { id_veiculos, valor_compra, data_compra } = request.body
+    const { id_veiculos, valor_compra } = request.body
     const compra = await prismaClient.compra.create({
       data: {
         valor_compra,
-        data_compra,
         id_veiculos
       }
     })
